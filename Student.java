bef0a9f9f74d27979a41f398c807a22bf8c33721
Yashara123 <saarasharindi@gmail.com>
2025-05-22 02:23:50 +0000

    save changes

--- Student.java
+++ Student.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 class Student{
 	private String name;
-	private int exam1;
-	private int exam2;
-	private int exam3;
+	private int e1;
+	private int e2;
+	private int e3;
 	
-	Student(String name,int exam1,int exam2,int exam3){
+	Student(String name,int e1,int e2,int e3){
 		this.name=name;
-		if (validateMarks(exam1)&&validateMarks(exam2)&&validateMarks(exam3)) {
-			this.exam1 = exam1;
-			this.exam2 = exam2;
-			this.exam3 = exam3;
+		if (validateMarks(e1)&&validateMarks(e2)&&validateMarks(e3)) {
+			this.e1 = e1;
+			this.e2 = e2;
+			this.e3 = e3;
 		}else{
 			throw new IllegalArgumentException("Marks must be between 0 and 100");
 		}
@@ -27,17 +27,17 @@ class Student{
 		}
 	}
 	public int getExam1(){
-		return exam1;
+		return e1;
 	}
 	public int getExam2(){
-		return exam2;
+		return e2;
 	}
 	public int getExam3(){
-		return exam3;
+		return e3;
 	}
 	
 	public double calculateAverage(){
-		double ave=(exam1+exam2+exam3)/3;
+		double ave=(e1+e2+e3)/3;
 		return ave;
 	}
 	
@@ -48,13 +48,13 @@ class Main{
 		System.out.print("Enter the Name :");
 		String name=sc.nextLine();
 		System.out.print("Enter marks for Exam1 :");
-		int exam1=sc.nextInt();
+		int e1=sc.nextInt();
 		System.out.print("Enter marks for Exam2 :");
-		int exam2=sc.nextInt();
+		int e2=sc.nextInt();
 		System.out.print("Enter marks for Exam3 :");
-		int exam3=sc.nextInt();
+		int e3=sc.nextInt();
 		try{
-			Student st=new Student(name,exam1,exam2,exam3);
+			Student st=new Student(name,e1,e2,e3);
 			System.out.println(name+",s Average mark is "+st.calculateAverage());
 		}catch(IllegalArgumentException e){
 			System.out.println("Error :"+ e.getMessage());
